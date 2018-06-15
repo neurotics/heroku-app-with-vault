eac3e9cf2736292b1693a5a49e2d3ed0f58f4fb6
Mars Hall <mars@heroku.com>
2018-06-15 11:50:17 -0700

    Display the `app_secrets` env var in web UI

--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,42 @@ import React from 'react'
 import Head from 'next/head'
 import Logo from '../components/logo'
 
-export default () => (
-  <div className="root">
-    <Head>
-      <meta charSet="utf-8"/>
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <title>Next.js on Heroku</title>
-    </Head>
-    <style jsx>{`
-      .root {
-        font-family: sans-serif;
-        line-height: 1.33rem;
-        margin-top: 8vh,
-      }
-      @media (min-width: 600px) {
+export default class extends React.Component {
+  static async getInitialProps({ req }) {
+    return {
+      secretsFromVault: JSON.parse(process.env.app_secrets)
+    }
+  }
+
+  render() {
+    return <div className="root">
+      <Head>
+        <meta charSet="utf-8"/>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Next.js on Heroku</title>
+      </Head>
+      <style jsx>{`
         .root {
-          margin-left: 21vw;
-          margin-right: 21vw;
+          font-family: sans-serif;
+          line-height: 1.33rem;
+          margin-top: 8vh,
         }
-      }
-    `}</style>
+        @media (min-width: 600px) {
+          .root {
+            margin-left: 21vw;
+            margin-right: 21vw;
+          }
+        }
+        pre {
+          font-size: 2rem;
+          line-height: 2rem;
+        }
+      `}</style>
 
-    <h1><Logo style={{ height: '1.45rem' }}/> Next.js on Heroku</h1>
+      <h1><Logo style={{ height: '1.45rem' }}/> Secrets from Vault</h1>
 
-    <p>Deploy <a href="https://nextjs.org/">Next.js</a> universal web apps on <a href="https://www.heroku.com/home">Heroku</a>.</p>
-
-    <p>This <strong>demo deployment on Heroku</strong> is from the repo <a href="https://github.com/mars/heroku-nextjs">mars/heroku-nextjs</a>.</p>
-
-    <p><a href="https://github.com/mars/heroku-nextjs/archive/master.zip">Download this Next.js app</a> as a Heroku-ready template, or follow <a href="https://github.com/mars/heroku-nextjs#production-deployment">Production Deployment</a> to push an existing app to Heroku.</p>
-  </div>
-)
+      <pre>{JSON.stringify(this.props.secretsFromVault, null, 2)}</pre>
+    </div>
+  }
+}
